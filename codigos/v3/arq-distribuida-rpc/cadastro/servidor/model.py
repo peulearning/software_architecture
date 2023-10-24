ffc41bc095df4156377636766aa379353b9fc06a
Pedro <peuhenry2018@gmail.com>
2023-10-24 18:45:19 -0300

    Primeiro Exercicio

--- codigos/v3/arq-distribuida-rpc/cadastro/servidor/model.py
+++ codigos/v3/arq-distribuida-rpc/cadastro/servidor/model.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import db
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Float, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 
 class Cliente(db.Base):
@@ -22,7 +22,7 @@ class Cliente(db.Base):
         """
             Representação do objeto com foco no programador.
         """
-        return '<Cliente: %s, %s, %s, %d, %s, %s>' % (self.nome, 
+        return '<Cliente: %s, %s, %s, %d, %s, %s>' % (self.nome,
                 self.cpf, self.dtNascimento, self.id, self.endereco,
                 self.dt_hr_manutencao)
 
@@ -37,8 +37,8 @@ class Endereco(db.Base):
     bairro = Column(String, nullable=False)
     cidade = Column(String, nullable=False)
     estado = Column(String, nullable=False)
-    cliente_id = Column(Integer, 
-                        ForeignKey("cliente.id", 
+    cliente_id = Column(Integer,
+                        ForeignKey("cliente.id",
                                     ondelete="CASCADE"),
                         )
     dt_hr_manutencao = Column(DateTime, default=datetime.now,
@@ -49,6 +49,42 @@ class Endereco(db.Base):
             Representação do objeto com foco no programador.
         """
         return '<Endereco: %s, %s, %s, %s, %s, %d, %d, %s>' % (
-            self.logradouro, self.numero, self.bairro, 
+            self.logradouro, self.numero, self.bairro,
             self.cidade, self.estado, self.id, self.cliente_id,
             self.dt_hr_manutencao)
+
+class Venda(db.Base):
+
+    __tablename__ = 'venda'
+
+    id = Column(Integer, primary_key=True)
+    cliente_id = Column(Integer, ForeignKey('cliente.id'), nullable=False)
+    valor = Column(Float, nullable=False)
+    data = Column(DateTime, nullable=False, default=datetime.now)
+    itens = relationship("ItemVenda", back_populates="venda", cascade="all, delete-orphan")
+
+    def __repr__(self):
+        """
+            Representação do objeto com foco no programador.
+        """
+        return f'<Venda: {self.id}, Cliente ID: {self.cliente_id}, Valor: {self.valor}, Data: {self.data}>'
+
+
+class ItemVenda(db.Base):
+
+    __tablename__ = 'item_venda'
+
+    id = Column(Integer, primary_key=True)
+    venda_id = Column(Integer, ForeignKey('venda.id'), nullable=False)
+    produto = Column(String, nullable=False)
+    quantidade = Column(Integer, nullable=False)
+    preco_unitario = Column(Float, nullable=False)
+
+    venda = relationship("Venda", back_populates="itens")
+
+    def __repr__(self):
+        """
+            Representação do objeto com foco no programador.
+        """
+        return f'<ItemVenda: {self.id}, Venda ID: {self.venda_id}, Produto: {self.produto}, Quantidade: {self.quantidade}, Preço Unitário: {self.preco_unitario}>'
+
